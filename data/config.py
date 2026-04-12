e196517d2d65e0ed534016dc84e43b4b54a9e2b6
Henry McGahan <vonstapler99@gmail.com>
2026-04-12 15:24:49 -0500

    Add initial project files including .gitignore, requirements, and bounding box config

---
+++ data/config.py
@@ -0,0 +1,8 @@
+#config.py
+# We are using a 2x2 mile bounding box around Kerrville, Texas
+
+30.052077, -99.144659
+
+CENTER_LAT = 30.052077
+CENTER_LON = -99.144659
+DIST_METERS = 1600 # 1 mile from center is roughly 2x2 miles
